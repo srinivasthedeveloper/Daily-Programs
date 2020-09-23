9a614e70ae8c29e76c6c789d3f74a9c65cb15147
SrinivasTheDeveloper <srinivasthedeveloper@gmail.com>
2020-09-23 07:52:34 +0530

    updated_american_keyboard

--- Day7/american keyboard solution.py
+++ Day7/american keyboard solution.py
@@ -1,13 +1,15 @@
-firstRow="qwertyuiop"
-secondRow="asdfghjkl"
-thirdRow="zxcvbnm"
+firstRow="`1234567890-="
+secondRow="qwertyuiop"
+thirdRow="asdfghjkl"
+fourthRow="zxcvbnm"
 flag=1
 List=list(map(str,input().split(',')))
 for i in List:
         f=sum([1 for _ in i.lower() if _ in firstRow])
         s=sum([1 for _ in i.lower() if _ in secondRow])
         t=sum([1 for _ in i.lower() if _ in thirdRow])
-        if(f==len(i) or s==len(i) or t==len(i)):
+        fr=sum([1 for _ in i.lower() if _ in thirdRow])
+        if(len(i)in[f,s,t,fr]):
                 print(i,end=" ")
                 flag=0
 print("-1")if(flag)else print("",end="")
