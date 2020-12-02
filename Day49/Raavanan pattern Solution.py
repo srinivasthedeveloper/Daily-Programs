dd6d98d35ab952f24f6cd2b9fcf15729f445dcd6
SrinivasTheDeveloper <srinivasthedeveloper@gmail.com>
2020-12-02 22:15:56 +0530

    Day49

---
+++ Day49/Raavanan pattern Solution.py
@@ -0,0 +1,21 @@
+n = int(input())
+print('(-'*(n//4),end='')
+print('(-_'*(n//4),end='')
+print('(-_-)',end='')
+print('_-)'*(n//4),end='')
+print('-)'*(n//4),end='')
+
+
+# n=int(input())
+# fh="(-"
+# ff="(-_"
+# lh="-)"
+# lf="_-)"
+# m="(-_-)"
+# t=(n-1)//2
+# th=t//2
+# print(fh*th,end="")
+# print(ff*th,end="")
+# print(m,end="")
+# print(lf*th,end="")
+# print(lh*th,end="")
