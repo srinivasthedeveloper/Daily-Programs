4f58a7eccd04145e56b4674f3a96e45429edcfda
SrinivasTheDeveloper <srinivasthedeveloper@gmail.com>
2021-01-09 19:32:28 +0530

    Day87

---
+++ Day87/Find the logic solution.py
@@ -0,0 +1,13 @@
+#Logic:- It is square of sum digits of n
+#Python type 1
+
+print(eval('+'.join(input()))**2)
+
+'''
+#Python type 2
+n=input().strip()
+s=0
+for i in n:
+	s+=int(i)
+print(s*s)
+'''
